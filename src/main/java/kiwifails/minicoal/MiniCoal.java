51521a7feced416a4d1c775f1191000b49de2efd
KiwiFails <jessheartsrandom@gmail.com>
2016-10-25 13:54:54 -0400

    Updated version number, derp

--- src/main/java/kiwifails/minicoal/MiniCoal.java
+++ src/main/java/kiwifails/minicoal/MiniCoal.java
@@ -17,7 +17,7 @@ import java.io.File;
 @Mod(modid = MiniCoal.MODID, version = MiniCoal.VERSION, name = MiniCoal.NAME)
 public class MiniCoal {
     public static final String MODID = "minicoal";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.0";
     public static final String NAME = "MiniCoal";
 
     @SidedProxy(clientSide = "kiwifails.minicoal.proxy.ClientProxy", serverSide = "kiwifails.minicoal.proxy.CommonProxy")
