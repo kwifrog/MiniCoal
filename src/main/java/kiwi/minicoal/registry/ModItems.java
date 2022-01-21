accf6f477a4a44593b999ca269c0fa52e8da15a6
Kiwi <6136683+KiwiOfLesbos@users.noreply.github.com>
2022-01-21 12:11:51 -0500

    Update to 1.18.1

--- src/main/java/kiwi/minicoal/registry/ModItems.java
+++ src/main/java/kiwi/minicoal/registry/ModItems.java
@@ -4,15 +4,15 @@ import kiwi.minicoal.MiniCoal;
 import kiwi.minicoal.items.FuelItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @SuppressWarnings("unused")
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MiniCoal.MODID);
 
-    public static final RegistryObject<Item> MINI_COAL = ITEMS.register(
+    public static final RegistryObject<FuelItem> MINI_COAL = ITEMS.register(
             "mini_coal", () -> new FuelItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
     public static final RegistryObject<Item> MINI_CHARCOAL = ITEMS.register(
             "mini_charcoal", () -> new FuelItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
